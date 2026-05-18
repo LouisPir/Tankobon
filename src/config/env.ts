d4d0b260d0ee3f238bf7fb72eab5e3b9ef4358ea
LouisPirot <152617560+LouisPir@users.noreply.github.com>
2026-05-18 14:38:05 +0200

    feat: configure environment variables with expo-constants (#25)
    Co-authored-by: Louis Pirot <louis.pirot@gmail.com>

---
+++ src/config/env.ts
@@ -0,0 +1,8 @@
+import Constants from 'expo-constants';
+
+const ENV = {
+  supabaseUrl: process.env.EXPO_PUBLIC_SUPABASE_URL ?? '',
+  supabaseAnonKey: process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY ?? '',
+};
+
+export default ENV;
