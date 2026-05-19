a73e797e7d86a44ee850406332572427463f646c
louis.pirot <louis.pirot@edu.devinci.fr>
2026-05-19 18:40:13 +0200

    problème de compatibilité entre la version d'AsyncStorage et Expo Go résolue

--- src/services/supabase.ts
+++ src/services/supabase.ts
@@ -1,10 +1,16 @@
 import { createClient } from '@supabase/supabase-js';
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import * as SecureStore from 'expo-secure-store';
 import ENV from '../config/env';
+
+const ExpoSecureStoreAdapter = {
+  getItem: (key: string) => SecureStore.getItemAsync(key),
+  setItem: (key: string, value: string) => SecureStore.setItemAsync(key, value),
+  removeItem: (key: string) => SecureStore.deleteItemAsync(key),
+};
 
 export const supabase = createClient(ENV.supabaseUrl, ENV.supabaseAnonKey, {
   auth: {
-    storage: AsyncStorage,
+    storage: ExpoSecureStoreAdapter,
     autoRefreshToken: true,
     persistSession: true,
     detectSessionInUrl: false,
